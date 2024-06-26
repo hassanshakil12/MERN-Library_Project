8e1e1ef147d715b8e13565396f5d9cc485e6f92d
Muhammad Hassan <hassanshakil12@gmail.com>
2024-06-27 04:38:21 +0500

    Home page completed

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,12 @@ import bookRoutes from "./routes/bookRoutes.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["content-type"]
-}))
+app.use(cors());
+// app.use(cors({
+//   origin: "",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["content-type"]
+// }))
 
 app.get("/", (req, res) => {
   return res.status(200).send("MERN-Stack Library");
